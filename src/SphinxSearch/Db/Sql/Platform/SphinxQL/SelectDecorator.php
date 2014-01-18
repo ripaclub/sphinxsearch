3bd3d2cc67bf21ca80b9c8b3e05ea63a54a7718d
Leonardo Grasso <me@leonardograsso.com>
2014-01-18 20:15:48 +0100

    SphinxQL draft

---
+++ src/SphinxSearch/Db/Sql/Platform/SphinxQL/SelectDecorator.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * ZF2 Sphinx Search
+ *
+ * @link        https://github.com/ripaclub/zf2-sphinxsearch
+ * @copyright   Copyright (c) 2014, Leonardo Di Donato <leodidonato at gmail dot com>, Leonardo Grasso <me at leonardograsso dot com>
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace SphinxSearch\Db\Sql\Platform\SphinxQL;
+
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\Adapter\Driver\DriverInterface;
+use Zend\Db\Adapter\ParameterContainer;
+use Zend\Db\Adapter\Platform\PlatformInterface;
+use Zend\Db\Sql\Platform\PlatformDecoratorInterface;
+use Zend\Db\Adapter\StatementContainerInterface;
+use Zend\Db\Sql\Select;
+
+class SelectDecorator extends Select implements PlatformDecoratorInterface
+{
+
+    /**
+     * @var Select
+     */
+    protected $select = null;
+
+    /**
+     * @param Select $select
+     */
+    public function setSubject($select)
+    {
+        $this->select = $select;
+    }
+
+    /**
+     * @see \Zend\Db\Sql\Select::renderTable
+     */
+    protected function renderTable($table, $alias = null)
+    {
+        return $table . ' ' . $alias;
+    }
+
+    /**
+     * @param AdapterInterface $adapter
+     * @param StatementContainerInterface $statementContainer
+     */
+    public function prepareStatement(AdapterInterface $adapter, StatementContainerInterface $statementContainer)
+    {
+        // localize variables
+        foreach (get_object_vars($this->select) as $name => $value) {
+            $this->{$name} = $value;
+        }
+        // set specifications
+        unset($this->specifications[self::LIMIT]);
+        unset($this->specifications[self::OFFSET]);
+
+        $this->specifications['LIMITOFFSET'] = null;
+        parent::prepareStatement($adapter, $statementContainer);
+    }
+
+    /**
+     * @param PlatformInterface $platform
+     * @return string
+     */
+    public function getSqlString(PlatformInterface $platform = null)
+    {
+        // localize variables
+        foreach (get_object_vars($this->select) as $name => $value) {
+            $this->{$name} = $value;
+        }
+
+        // set specifications
+        unset($this->specifications[self::LIMIT]);
+        unset($this->specifications[self::OFFSET]);
+
+        $this->specifications['LIMITOFFSET'] = null;
+        return parent::getSqlString($platform);
+    }
+
+    /**
+     * @param PlatformInterface $platform
+     * @param DriverInterface $driver
+     * @param ParameterContainer $parameterContainer
+     * @param $sqls
+     * @param $parameters
+     * @return null
+     */
+    protected function processLimitOffset(PlatformInterface $platform, DriverInterface $driver = null, ParameterContainer $parameterContainer = null, &$sqls, &$parameters)
+    {
+        if ($this->limit === null && $this->offset === null) {
+            return null;
+        }
+
+        if ($this->offset === null) {
+            $this->offset = 0;
+        }
+
+        if ($this->limit === null) {
+            $this->limit = 0;
+        }
+
+        if ($driver) {
+            $sql = $driver->formatParameterName('offset') . ',' . $driver->formatParameterName('limit');
+            $parameterContainer->offsetSet('limit', $this->limit, ParameterContainer::TYPE_INTEGER);
+            $parameterContainer->offsetSet('offset', $this->offset, ParameterContainer::TYPE_INTEGER);
+        } else {
+            $sql = $this->offset . ', ' .$this->limit;
+        }
+
+        return array($sql);
+    }
+
+}
