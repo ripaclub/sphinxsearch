d60a1bf7f5d0c154061e9fbafd8e8d8ef240568e
Leonardo Grasso <me@leonardograsso.com>
2014-01-20 17:51:06 +0100

    Moving from Platform/Decorator to new Sql class implementation

---
+++ src/SphinxSearch/Db/Sql/Sql.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * ZF2 Sphinx Search
+ *
+ * @link        https://github.com/ripaclub/zf2-sphinxsearch
+ * @copyright   Copyright (c) 2014, Leonardo Di Donato <leodidonato at gmail dot com>, Leonardo Grasso <me at leonardograsso dot com>
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+namespace SphinxSearch\Db\Sql;
+
+use Zend\Db\Sql\Sql as ZendSql;
+
+class Sql extends ZendSql
+{
+    public function select($table = null)
+    {
+        if ($this->table !== null && $table !== null) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'This Sql object is intended to work with only the table "%s" provided at construction time.',
+                $this->table
+            ));
+        }
+        return new Select(($table) ?: $this->table);
+    }
+}
