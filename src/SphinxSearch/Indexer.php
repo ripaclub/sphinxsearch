17b0b09349cc4d0f06cbab7b14b8aaa09e173c76
Leonardo Grasso <me@leonardograsso.com>
2014-01-24 15:40:57 +0100

    Indexer class draft

--- src/SphinxSearch/Indexer.php
+++ src/SphinxSearch/Indexer.php
@@ -8,12 +8,64 @@
  */
 
 namespace SphinxSearch;
+use Zend\Db\Adapter\Adapter as ZendDBAdapter;
+use SphinxSearch\Db\Sql\Sql;
+use SphinxSearch\Db\Sql\Replace;
 
-class SphinxIndexer {
+class Indexer {
 
-    public function __construct()
+    /**
+     * @var ZendDBAdapter
+     */
+    protected $adapter;
+
+    /**
+     * @var Sql
+     */
+    protected $sql;
+
+    /**
+     * @param ZendDBAdapter $adapter
+     * @param Sql $sql
+     */
+    public function __construct(ZendDBAdapter $adapter, Sql $sql = null)
+    {
+        $this->adapter = $adapter;
+
+        $this->sql     = $sql ? $sql : new Sql($adapter);
+    }
+
+    /**
+     * @return Sql
+     */
+    public function getSql()
+    {
+        return $this->sql;
+    }
+
+    public function beginTransaction()
+    {
+        $this->adapter->driver->getConnection()->beginTransaction();
+        return $this;
+    }
+
+    public function commit()
+    {
+        $this->adapter->driver->getConnection()->commit();
+        return $this;
+    }
+
+    public function rollback()
+    {
+        $this->adapter->driver->getConnection()->rollback();
+        return $this;
+    }
+
+    public function update(array $data, $index, $upsert = true)
     {
 
     }
+
+
 
 }
