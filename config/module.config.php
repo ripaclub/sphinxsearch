9bf429c5b3549e0707fdf8415ccf70c599c6dbd1
leodido <leodidonato@gmail.com>
2014-01-28 01:36:10 +0100

    Removed AdapterServiceFactory

--- config/module.config.php
+++ config/module.config.php
@@ -4,11 +4,5 @@ return array(
         'abstract_factories' => array(
             'SphinxSearch\Db\Adapter\AdapterAbstractServiceFactory'
         ),
-        'factories' => array(
-            'SphinxSearch\Db\Adapter\Adapter' => 'SphinxSearch\Db\Adapter\AdapterServiceFactory',
-        ),
-        'alias' => array(
-            'sphinxql' => 'SphinxSearch\Db\Adapter\Adapter'
-        ),
     ),
 );
