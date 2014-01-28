9271765ccc853d9114e5d15ddfe74b8093c94d49
leodido <leodidonato@gmail.com>
2014-01-28 16:02:01 +0100

    Added test for SphinxQL platform

---
+++ tests/SphinxSearchTests/Db/Adapter/Platform/SphinxQLTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * User: leodido
+ * Date: 28/01/14
+ * Time: 15.59
+ */
+
+namespace SphinxSearchTests\Platform;
+
+use SphinxSearch\Db\Adapter\Platform\SphinxQL;
+
+class SphinxQLTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var SphinxQL
+     */
+    protected $platform;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->platform = new SphinxQL;
+    }
+
+    /**
+     * @covers SphinxSearch\Db\Adapter\Platform\SphinxQL::getName
+     */
+    public function testGetName()
+    {
+        $this->assertEquals('SphinxQL', $this->platform->getName());
+    }
+
+}
+ 
