d4c8c660ea91d1ba1836bc82d00fc9123a08bc6f
leodido <leodidonato@gmail.com>
2014-01-28 18:26:07 +0100

    Update test created

--- tests/SphinxSearchTests/Db/Sql/SqlTest.php
+++ tests/SphinxSearchTests/Db/Sql/SqlTest.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * User: leodido
- * Date: 28/01/14
- * Time: 16.09
+ * ZF2 Sphinx Search
+ *
+ * @link        https://github.com/ripaclub/zf2-sphinxsearch
+ * @copyright   Copyright (c) 2014, Leonardo Di Donato <leodidonato at gmail dot com>, Leonardo Grasso <me at leonardograsso dot com>
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
  */
-
 namespace SphinxSearchTests\Db\Sql;
 
 use SphinxSearch\Db\Sql\Sql;
