b13daa1256595187887ef72c46eb7f7f9e617ac9
leodido <leodidonato@gmail.com>
2014-01-29 12:30:32 +0100

    Started tests for Sql class

--- tests/SphinxSearchTests/Db/Sql/SqlTest.php
+++ tests/SphinxSearchTests/Db/Sql/SqlTest.php
@@ -8,93 +8,50 @@
  */
 namespace SphinxSearchTests\Db\Sql;
 
+
+
 use SphinxSearch\Db\Sql\Sql;
-use Zend\Db\Adapter\AdapterInterface;
-use Zend\ServiceManager\ServiceManager;
-use Zend\ServiceManager\Config;
 
 class SqlTest extends \PHPUnit_Framework_TestCase
 {
 
-    /**
-     * @var \Zend\ServiceManager\ServiceLocatorInterface
-     */
-    private $serviceManager;
-
-    /** @var AdapterInterface */
-    protected $adapter = null;
+    protected $mockAdapter = null;
 
     /**
+     * Sql object
      * @var Sql
      */
     protected $sql = null;
 
-    protected function setUp()
+    public function setup()
     {
-        $this->serviceManager = new ServiceManager(new Config(array(
-            'factories' => array(
-                'SphinxSearch\Db\Adapter\Adapter' => 'SphinxSearch\Db\Adapter\AdapterServiceFactory'
-            )
-        )));
-        $this->serviceManager->setService('Config', array(
-                'sphinxql' => array(
-                    'driver' => 'pdo_mysql',
-                ),
-        ));
-        $this->adapter = $this->serviceManager->get('SphinxSearch\Db\Adapter\Adapter');
-        $this->sql = new Sql($this->adapter);
+        // mock the adapter, driver, and parts
+        $mockResult = $this->getMock('Zend\Db\Adapter\Driver\ResultInterface');
+        $mockStatement = $this->getMock('Zend\Db\Adapter\Driver\StatementInterface');
+        $mockStatement->expects($this->any())->method('execute')->will($this->returnValue($mockResult));
+        $mockConnection = $this->getMock('Zend\Db\Adapter\Driver\ConnectionInterface');
+        $mockDriver = $this->getMock('Zend\Db\Adapter\Driver\DriverInterface');
+        $mockDriver->expects($this->any())->method('createStatement')->will($this->returnValue($mockStatement));
+        $mockDriver->expects($this->any())->method('getConnection')->will($this->returnValue($mockConnection));
+        // setup mock adapter
+        $this->mockAdapter = $this->getMock('Zend\Db\Adapter\Adapter', null, array($mockDriver));
+
+        $this->sql = new Sql($this->mockAdapter, 'foo'); // FIXME: append SphinxQL platform?
     }
 
     /**
      * @covers SphinxSearch\Db\Sql\Sql::select
-     * @testdox Select table
      */
     public function testSelect()
     {
-        $table = 'foo';
         $select = $this->sql->select();
         $this->assertInstanceOf('SphinxSearch\Db\Sql\Select', $select);
-        $this->assertSame($table, $select->getRawState('table'));
+        $this->assertSame('foo', $select->getRawState('table'));
 
         $this->setExpectedException(
             'SphinxSearch\Db\Sql\Exception\InvalidArgumentException',
-            'This Sql object is intended to work with only the table "' . $table . '" provided at construction time.'
-        );
+            'This Sql object is intended to work with only the table "foo" provided at construction time.');
         $this->sql->select('bar');
-    }
-
-    /**
-     * @covers SphinxSearch\Db\Sql\Sql::replace
-     * @testdox Replace values into table
-     */
-    public function testReplace()
-    {
-        $table = 'foo';
-        $replace = $this->sql->replace();
-        $this->assertInstanceOf('SphinxSearch\Db\Sql\Replace', $replace);
-        $this->assertSame($table, $replace->getRawState('table'));
-
-        $this->setExpectedException(
-            'SphinxSearch\Db\Sql\Exception\InvalidArgumentException',
-            'This Sql object is intended to work with only the table "' . $table . '" provided at construction time.');
-        $this->sql->insert('bar');
-    }
-
-    /**
-     * @covers SphinxSearch\Db\Sql\Sql::update
-     * @testdox Update table
-     */
-    public function testUpdate()
-    {
-        $table = 'foo';
-        $update = $this->sql->update();
-        $this->assertInstanceOf('SphinxSearch\Db\Sql\Update', $update);
-        $this->assertSame($table, $update->getRawState('table'));
-
-        $this->setExpectedException(
-            'SphinxSearch\Db\Sql\Exception\InvalidArgumentException',
-            'This Sql object is intended to work with only the table "' . $table . '" provided at construction time.');
-        $this->sql->update('bar');
     }
 
 }
