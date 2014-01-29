71e5945ca7e16038e5417253f3fa2de56d59f3c4
leodido <leodidonato@gmail.com>
2014-01-29 19:25:06 +0100

    Started indexer test class

---
+++ tests/SphinxSearchTests/IndexerTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ZF2 Sphinx Search
+ *
+ * @link        https://github.com/ripaclub/zf2-sphinxsearch
+ * @copyright   Copyright (c) 2014, Leonardo Di Donato <leodidonato at gmail dot com>, Leonardo Grasso <me at leonardograsso dot com>
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+namespace SphinxSearchTests;
+
+class IndexerTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
