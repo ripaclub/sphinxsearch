f06789929cc9cf000c4cf414854cdcf3a7a86ebe
leodido <leodidonato@gmail.com>
2014-01-29 10:58:49 +0100

    Added trusted platform

---
+++ tests/SphinxSearchTests/Db/TestAsset/TrustedSphinxQL.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ZF2 Sphinx Search
+ *
+ * @link        https://github.com/ripaclub/zf2-sphinxsearch
+ * @copyright   Copyright (c) 2014, Leonardo Di Donato <leodidonato at gmail dot com>, Leonardo Grasso <me at leonardograsso dot com>
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+namespace SphinxSearchTests\Db\TestAsset;
+
+use SphinxSearch\Db\Adapter\Platform\SphinxQL;
+
+class TrustedSphinxQL extends SphinxQL {
+
+    public function quoteValue($value)
+    {
+        return $this->quoteTrustedValue($value);
+    }
+
+} 
