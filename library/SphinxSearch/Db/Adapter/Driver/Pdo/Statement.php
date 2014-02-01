fd1ee74b0798f68eda101dbce27c0dac2fe881f9
leodido <leodidonato@gmail.com>
2014-02-01 17:04:03 +0100

    Library fixes

--- library/SphinxSearch/Db/Adapter/Driver/Pdo/Statement.php
+++ library/SphinxSearch/Db/Adapter/Driver/Pdo/Statement.php
@@ -6,7 +6,6 @@
  * @copyright   Copyright (c) 2014, Leonardo Di Donato <leodidonato at gmail dot com>, Leonardo Grasso <me at leonardograsso dot com>
  * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
  */
-
 namespace SphinxSearch\Db\Adapter\Driver\Pdo;
 
 use Zend\Db\Adapter\Driver\Pdo\Statement as ZendPdoStatement;
