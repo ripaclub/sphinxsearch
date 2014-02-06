31811013bb735a0cc41cae186fac0c2a7ac5f023
Leonardo Grasso <me@leonardograsso.com>
2014-02-06 02:18:05 +0100

    Fixed repository URL

--- library/SphinxSearch/Db/Sql/Predicate/Match.php
+++ library/SphinxSearch/Db/Sql/Predicate/Match.php
@@ -2,7 +2,7 @@
 /**
  * Sphinx Search
  *
- * @link        https://github.com/ripaclub/zf2-sphinxsearch
+ * @link        https://github.com/ripaclub/sphinxsearch
  * @copyright   Copyright (c) 2014, Leonardo Di Donato <leodidonato at gmail dot com>, Leonardo Grasso <me at leonardograsso dot com>
  * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
  */
