2d241f6571544b4bb812b2980d5d19c1b8e3afd2
Lorenzo Fontana <fontanalorenzo@me.com>
2014-04-03 15:39:34 +0200

    WTF

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Matryoshka
+ * Sphinx Search
  *
- * @link        https://github.com/ripaclub/matryoshka
- * @copyright   Copyright (c) 2014, Ripa Club <ripaclub@gmail.com>
+ * @link        https://github.com/ripaclub/sphinxsearch
+ * @copyright   Copyright (c) 2014,
+ *              Leonardo Di Donato <leodidonato at gmail dot com>
+ *              Leonardo Grasso <me at leonardograsso dot com>
  * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
  */
-
 
 chdir(__DIR__);
 setlocale(LC_NUMERIC, 'C');
