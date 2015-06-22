5758c14a3626b09d8368b10f361e5b1c67ae1b8c
Leonardo Grasso <me@leonardograsso.com>
2015-06-22 11:47:39 +0200

    Copyright (c) 2014-2015

--- library/Db/Adapter/AdapterAbstractServiceFactory.php
+++ library/Db/Adapter/AdapterAbstractServiceFactory.php
@@ -3,7 +3,7 @@
  * Sphinx Search
  *
  * @link        https://github.com/ripaclub/sphinxsearch
- * @copyright   Copyright (c) 2014,
+ * @copyright   Copyright (c) 2014-2015
  *              Leo Di Donato <leodidonato at gmail dot com>,
  *              Leonardo Grasso <me at leonardograsso dot com>
  * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
